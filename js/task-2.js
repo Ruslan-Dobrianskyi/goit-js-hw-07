048e28fb01a3b2bf7b7fe10a1d3da8740b7d01ed
Ruslan-Dobrianskyi <130309553+Ruslan-Dobrianskyi@users.noreply.github.com>
2024-02-21 18:11:42 +0200

    homework 7 task 1 2

---
+++ js/task-2.js
@@ -0,0 +1,52 @@
+// Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+// <ul class="gallery"></ul>
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Elephant Beside on Baby Elephant",
+  },
+  {
+    url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Blue Geeen and Orange Parrot",
+  },
+  {
+    url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Zebras on Zebra",
+  },
+];
+const colorPickerContainerEl = document.querySelector(".gallery");
+
+const myElems = [];
+
+for (const obj of images) {
+  const myElem = document.createElement("li");
+
+  const firstChild = document.createElement("img");
+  firstChild.style.width = "200px";
+  firstChild.style.height = "200px";
+  firstChild.src = obj.url;
+  firstChild.alt = obj.alt;
+
+  myElem.append(firstChild);
+
+  myElems.push(myElem);
+}
+
+colorPickerContainerEl.append(...myElems);
